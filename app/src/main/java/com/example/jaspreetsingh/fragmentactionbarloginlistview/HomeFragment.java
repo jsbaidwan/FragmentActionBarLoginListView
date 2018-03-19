7cd5f38bb8ef315051e5e3133ced027459cb2658
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-03-19 10:43:52 +0530

    ADD: activity category with id:container

--- app/src/main/java/com/example/jaspreetsingh/fragmentactionbarloginlistview/HomeFragment.java
+++ app/src/main/java/com/example/jaspreetsingh/fragmentactionbarloginlistview/HomeFragment.java
@@ -22,9 +22,11 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View rootView = inflater.inflate(R.layout.activity_home, container, false);
+
+        //getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        return rootView;
     }
 
 }
