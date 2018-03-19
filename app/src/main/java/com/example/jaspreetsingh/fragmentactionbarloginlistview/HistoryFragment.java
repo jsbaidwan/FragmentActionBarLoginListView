5a9a43a61137ba0afde2294276092a974b517423
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-03-19 10:54:40 +0530

    NEW: History Fragment

---
+++ app/src/main/java/com/example/jaspreetsingh/fragmentactionbarloginlistview/HistoryFragment.java
@@ -0,0 +1,32 @@
+package com.example.jaspreetsingh.fragmentactionbarloginlistview;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_history, container, false);
+
+        //getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        return rootView;
+    }
+
+}
